e04b9a0ebde620863540b3288c08e8d0616f9587
Simon Porritt <simon@jsplumbtoolkit.com>
2025-04-21 13:20:00 +1000

    - use the new InspectorComponent which provides a far easier interface. - update the code to use angular to keep the task progress label in sync with the slider

--- angular/src/app/gantt.inspector.ts
+++ angular/src/app/gantt.inspector.ts
@@ -1,83 +1,60 @@
-import {AfterViewInit, ChangeDetectorRef, Component, ElementRef, inject, Input} from "@angular/core"
-import {Base, Inspector } from "@jsplumbtoolkit/browser-ui"
-import {jsPlumbService} from "@jsplumbtoolkit/browser-ui-angular"
-
+import {AfterViewInit, Component, Input, OnInit} from "@angular/core"
+import {Surface } from "@jsplumbtoolkit/browser-ui"
+import {InspectorComponent} from "@jsplumbtoolkit/browser-ui-angular"
 
 @Component({
   template: `<div class="inspector">
-        
-        <div *ngIf="currentType === ''"></div>
     
-        <div *ngIf="currentType === 'task'" class="jtk-gantt-inspector jtk-task-inspector">
-          
+        @if(currentType === 'task') {
+          <div class="jtk-gantt-inspector jtk-task-inspector">
+            
+              <div>Name</div>
+              <input type="text" jtk-att="name" jtk-focus/>
+              <div>Progress</div>
+              <div style="display:flex;align-items:center">
+                <input type="range" jtk-att="progress" min="0" max="100" value="{{currentObj.data.progress}}" (input)="updateRangeLabel($event)"/>
+                <div class="jtk-gantt-progress-value-label">{{currentObj.data.progress}}</div>
+              </div>
+          </div>
+        }
+
+        @if(currentType === 'taskGroup') {
+          <div class="jtk-gantt-inspector jtk-task-group-inspector">
             <div>Name</div>
             <input type="text" jtk-att="name" jtk-focus/>
-            <div>Progress</div>
-            <div style="display:flex;align-items:center">
-              <input type="range" jtk-att="progress" min="0" max="100" value="{{obj.data.progress}}"/>
-              <div class="jtk-gantt-progress-value-label">{{obj.data.progress}}</div>
-            </div>
-        </div>
+          </div>
+        }  
 
-        <div *ngIf="currentType === 'taskGroup'" class="jtk-gantt-inspector jtk-task-group-inspector">
-          <div>Name</div>
-          <input type="text" jtk-att="name" jtk-focus/>
-        </div>
-
-        <div *ngIf="currentType === 'milestone'" class="jtk-gantt-inspector jtk-task-milestone-inspector">
-          <div>Name</div>
-          <input type="text" jtk-att="name" jtk-focus/>
-        </div>
+        @if(currentType === 'milestone') {
+          <div class="jtk-gantt-inspector jtk-task-milestone-inspector">
+            <div>Name</div>
+            <input type="text" jtk-att="name" jtk-focus/>
+          </div>
+        }  
     
     </div>`,
   selector: 'jtk-gantt-inspector'
 })
-export class GanttInspector implements AfterViewInit {
+export class GanttInspector extends InspectorComponent implements OnInit {
 
   @Input() chartId!:string
-  inspector!: Inspector
-  $jsplumb = inject(jsPlumbService)
-  currentType = ''
-  obj!:Base
 
-  constructor(private el: ElementRef, private changeDetector: ChangeDetectorRef) { }
+  ngOnInit() {
+    this.surfaceId = this.chartId
+  }
 
-  ngAfterViewInit(): void {
+  // updates the label next to the range slider for a task.
+  updateRangeLabel(e:Event) {
+    const el = e.target as HTMLInputElement
+    const newValue = el.value
+    el.nextElementSibling && (el.nextElementSibling.innerHTML = `${newValue}`)
+  }
 
-    this.$jsplumb.getSurface(this.chartId, (surface) => {
-      this.inspector = new Inspector({
-        container: this.el.nativeElement,
-        surface,
-        renderEmptyContainer: () => {
-          this.currentType = '';
-          this.changeDetector.detectChanges()
-        },
-        refresh: (obj: Base, cb: () => void) => {
-          this.obj = obj
-          this.currentType = obj.data.type;
-          setTimeout(cb, 0);
-          this.changeDetector.detectChanges();
-        },
-        afterUpdate:() => surface.relayout()
-      });
-
-      // JsPlumb's inspectors don't natively support the range element currently, so we listen for change events
-      // and invoke an update manually. From 6.24.0 onwards this will not be necessary.
-      surface.on(this.el.nativeElement, "change", "[type='range']", (e:MouseEvent) => {
-        const newValue = (e.target as any).value
-        this.inspector.setValue("progress", newValue)
-      })
-
-      /**
-       * Update the label next to the slider to show the current percentage as the slider is dragged.
-       */
-      surface.on(this.el.nativeElement, "input", "[type='range']", (e:MouseEvent) => {
-        const t = e.target as HTMLInputElement
-        const newValue = t.value
-        t.nextElementSibling && (t.nextElementSibling.innerHTML = `${newValue}`)
-      })
-
-    });
+  /**
+   * We relayout after an update because that causes the decoration to be repainted.
+   */
+  override afterUpdate(surface:Surface) {
+    surface.relayout()
   }
 
 
